3230e78de1799fc3458a45fffe69d0ff5f500140
IT24100642 <IT24100642@my.sliit.lk>
2025-10-02 19:28:20 +0530

    Add files via upload

---
+++ Lab Sheet 09/LAB09_IT24100642/IT24100642_Rfile.R
@@ -0,0 +1,10 @@
+setwd("C:\\Users\\Neth\\Desktop\\LAB09_IT24100642")
+getwd()
+#1
+#(i)
+set.seed(123)
+bake_time<-rnorm(25,mean=45,sd=2)
+print(bake_time)
+#(ii)
+t.test(bake_time,mu=46,alternative = "less",conf.level = 0.95)
+print(t.test)
